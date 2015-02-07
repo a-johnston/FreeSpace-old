3d9246b623d44407479a629200a0dd8af64d9892
Joshua Tang <jlt81@case.edu>
2015-02-07 12:41:38 -0500

    shit

--- java/psiborg/freespace/WorldActivity.java
+++ java/psiborg/freespace/WorldActivity.java
@@ -15,12 +15,12 @@ import psiborg.android5000.util.Vector3;
 public class WorldActivity extends Activity {
     Android5000 render;
     Sensors sensors;
-    CameraInput camin;
+    //CameraInput camin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         render = new Android5000(this);
-        camin  = new CameraInput(this);
+        //camin  = new CameraInput(this);
 
         sensors = new Sensors(this);
         sensors.start();
