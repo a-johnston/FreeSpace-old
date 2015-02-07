e11ade13571043dfb80f99ac0555c2ded0666c61
Adam J <amj69@case.edu>
2015-02-06 22:49:02 -0500

    pulled in test assets

--- java/psiborg/freespace/Sensors.java
+++ java/psiborg/freespace/Sensors.java
@@ -1,13 +1,25 @@
 package psiborg.freespace;
 
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
-public class Sensors {
+public class Sensors implements SensorEventListener {
     private SensorManager man;
     public Sensors(Context context) {
         man  = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         Log.e("sensors", man.toString());
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
--- java/psiborg/freespace/WorldActivity.java
+++ java/psiborg/freespace/WorldActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import psiborg.android5000.Android5000;
+import psiborg.android5000.Suzanne;
+import psiborg.android5000.TouchCamera;
+import psiborg.android5000.base.Scene;
 
 
 public class WorldActivity extends Activity {
@@ -12,6 +15,11 @@ public class WorldActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         render = new Android5000(this);
+
+        Scene scene = new Scene();
+        scene.add(new TouchCamera());
+        scene.add(new Suzanne());
+        render.setScene(scene);
     }
 
 
